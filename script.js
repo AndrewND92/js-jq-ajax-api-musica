fa99963ddc3ccfc6f3286c705e0d9dd84df86498
Andrea <miranda.andrea.nd@gmail.com>
2020-07-22 18:03:23 +0200

    si vede la funzione    w(e, t)    in console

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 
-$.ajax(
-  {
-    url: "https://flynn.boolean.careers/exercises/api/array/music",
-    method: "GET",
-    success: function (data) {
 
-    $("").html(data);
-    },
+function mainAjax() {
 
-    error: function (error) {
-    alert("E' avvenuto un errore. " + error);
-    }
-  }
-);
+        $.ajax (
+          {
+            url: "https://flynn.boolean.careers/exercises/api/array/music",
+            method: "GET",
+
+            success: function (data,stato) {
+              console.log(data);
+
+              var albums = data.response;
+              // console.log(albums);
+
+              for (var album in album) {
+                if (object.hasOwnProperty(variable)) {
+
+                }
+              }
+            },
+
+            error: function (error) {
+              alert("E' avvenuto un errore. " + error);
+            }
+          }
+        );
+      }
 
 
-
+  $(document).ready(function(mainAjax) {
+    console.log(mainAjax);
+  });
 
 
 
